aaec0ba95ecc52b733c0d35fc51de02cb511b6f3
Jason Buckner <jason@yola.com>
2012-11-09 11:10:38 -0800

    version bump to 2.2.5.yola3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 if 'sdist' in sys.argv:
     os.system('./admin/makedoc')
 
-version = '[library version:2.2.5]'[17:-1]
+version = '[library version:2.2.5.yola3]'[17:-1]
 
 setup(
     name='python-openid',
